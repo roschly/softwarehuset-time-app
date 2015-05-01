8e1a3a773dcc0cbf2267cb238eba8c944b857a9f
josefinetusindfryd <josefinetusindfryd@gmail.com>
2015-05-01 20:13:03 +0200

    Test divided to match use cases. Needs more sorting

--- softwarehuset-time-app/test/sh/app/TestProjectManagement.java
+++ softwarehuset-time-app/test/sh/app/TestProjectManagement.java
@@ -13,8 +13,62 @@ import static org.junit.Assert.fail;
 
 public class TestProjectManagement {
 	
+	//Where to put this
+	@Test
+	public void testGetEstimatedTime() throws Exception{
+		Project project = new Project("p1","2015-01", "2015-04");
+		Task task = new Task(project, "taskname", 5.5,"2015-02", "2015-03");
+		
+		assertTrue(task.getEstimatedTime() == 5.5);
+	}
 	
-	// Basic functionality 
+	@Test
+	public void testGetDeveloper() throws Exception {
+		Project project = new Project("p1","2015-01", "2015-04");
+		Task task = new Task(project, "taskname", 5.5,"2015-02", "2015-03"); 
+		User user = new User("dev");
+		Activity activity = new Activity("2015-01-01", 5.0, user, task);
+		
+		// Correct user
+		assertEquals(activity.getDeveloper(), user); 
+		
+		// Wrong user
+		User wrongUser = new User("dev1");
+		assertNotEquals(activity.getDeveloper(), wrongUser); 
+	}
+	
+	@Test
+	public void testGetDate() throws Exception {
+		Project project = new Project("p1","2015-01", "2015-04");
+		Task task = new Task(project, "taskname", 5.5,"2015-02", "2015-03"); 
+		User user = new User("dev");
+		String date = "2015-01-01"; 
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		format.setLenient(false);
+		
+		Activity activity = new Activity(date, 5.0, user, task);
+		
+		// Correct date
+		assertEquals(activity.getDate(), format.parse(date)); 
+		
+		// Different date
+		String differentDate = "2015-03-01"; 
+		assertNotEquals(activity.getDate(), format.parse(differentDate)); 
+	}
+	
+	@Test
+	public void testGetEndDate() throws Exception{
+		String startDate = "2014-01"; 
+		String endDate = "2015-01"; 
+		SimpleDateFormat format = new SimpleDateFormat("YYYY-ww");
+		format.setLenient(false);
+		DateObject dateObject = new DateObject(startDate, endDate); 
+		
+		assertEquals(dateObject.getEndDate(), format.parse(endDate)); 
+		
+	}
+
+	// TestBasicFunctionality 
 	// The test of dates in both project and task are collected.
 	// since they both extend DateObject this is tested instead
 	
@@ -121,6 +175,7 @@ public class TestProjectManagement {
 			assertEquals(e.getOperation(), "Set enddate");
 		}
 	}
+	
 	// TestProjectManagement
 	
 	// Test creation of project
@@ -141,8 +196,7 @@ public class TestProjectManagement {
 			assertEquals(e.getOperation(), "Construct project"); 
 		}
 	}
-	
-	// Test add project to timeApp 
+	 
 	@Test
 	public void testAddProject() throws Exception {
 		TimeApp timeApp = new TimeApp();
@@ -166,6 +220,38 @@ public class TestProjectManagement {
 		}		
 		
 	}
+	
+	@Test 
+	public void testGetAndSetProjectManager() throws Exception{
+		Project project = new Project("p1","2015-01", "2015-02");  
+		User user = new User("dev");
+		
+		project.setProjectmanager(user);
+		
+		assertEquals(project.getProjectmanager(), user); 
+	}
+	
+	@Test
+	public void testGetAndAddTask() throws Exception{
+		Project project = new Project("p1","2015-01", "2015-02"); 
+		String taskName = "taskname"; 
+		
+		// Test add task with unique name
+		assertEquals(project.getTasks().size(), 0); 
+		Task task = new Task(project,taskName, 5.0, "2015-01", "2015-02"); 
+		assertEquals(project.getTasks().size(), 1); 
+		
+		// Test add task with not unique name
+		try {
+			Task task1 = new Task(project,taskName, 1.0, "2015-11", "2015-20");
+			fail("OperationNotAllowedException should have been thrown"); 
+		} catch (OperationNotAllowedException e) {
+			assertEquals(e.getMessage(), "Task name must be unique"); 
+			assertEquals(e.getOperation(), "Add task"); 
+		}
+		
+	}
+
 	
 	@Test
 	public void selectProject() throws Exception {
@@ -227,7 +313,7 @@ public class TestProjectManagement {
 	}
 	
 	@Test
-	public void testGetUsers() throws Exception{
+	public void testGetDevelopers() throws Exception{
 		TimeApp timeApp = new TimeApp(); 
 		User user = new User("dev");
 		
@@ -241,7 +327,7 @@ public class TestProjectManagement {
 	}
 	
 	@Test
-	public void getUserByName() throws Exception{
+	public void testSelectDeveloper() throws Exception{
 		TimeApp timeApp = new TimeApp();
 		User user = new User("dev");
 		timeApp.addUser(user);
@@ -254,11 +340,9 @@ public class TestProjectManagement {
 		User testUser2 = timeApp.getUserByName( "wrong name" );
 		assertEquals(testUser2, null);
 	}
-
-
 	
 	@Test
-	public void selectProjectAndTaskAndAddDeveloper() throws Exception {
+	public void testAssignDeveloper() throws Exception {
 		TimeApp timeApp = new TimeApp();
 		Project project = new Project("p1", "2015-01", "2015-02");
 		timeApp.addProject(project);
@@ -287,5 +371,116 @@ public class TestProjectManagement {
 		
 	}
 	
+	// TestRegisterTime
+	
+	@Test
+	public void testSelectTask() throws Exception{
+		Project project = new Project("p1","2015-01", "2015-02"); 
+		String taskName = "taskname"; 
+		Task task = new Task(project,taskName, 5.0, "2015-01", "2015-02");
+		assertEquals(project.getTaskByName(taskName), task);
+	}
+	
+	@Test
+	public void testCreateActivity() throws Exception{
+		Project project = new Project("p1","2015-01", "2015-04");
+		Task task = new Task(project, "taskname", 5.5,"2015-02", "2015-03"); 
+		User user = new User("dev");
 		
+		
+		// Test add activity
+		assertEquals(task.getActivities().size(), 0);
+		Activity activity = new Activity("2015-01-01", 5.0, user, task);
+		assertEquals(task.getActivities().size(), 1);
+		SimpleDateFormat format = new SimpleDateFormat("YYYY-MM-DD");
+		
+		// Test wrong date format
+		String wrongDate = "1"; 
+		try {
+			Activity activityWrongDate = new Activity(wrongDate, 5.0, user, task);
+			fail("OperationNotAllowedException should have been thrown"); 
+		} catch (OperationNotAllowedException e) {
+			assertEquals(e.getMessage(), "Date must have the format yyyy-MM-dd"); 
+			assertEquals(e.getOperation(),"Set date"); 
+		}
+		
+		assertEquals(task.getActivities().size(), 1);
+		
+		// Test wrong date format
+		wrongDate = "01-01-2014"; 
+		try {
+			Activity activityWrongDate = new Activity(wrongDate, 5.0, user, task);
+			fail("OperationNotAllowedException should have been thrown"); 
+		} catch (OperationNotAllowedException e) {
+			assertEquals(e.getMessage(), "Date must have the format yyyy-MM-dd"); 
+			assertEquals(e.getOperation(),"Set date"); 
+		}
+		
+		assertEquals(task.getActivities().size(), 1);
+		
+		// Test null date
+		wrongDate = null; 
+		try {
+			Activity activityWrongDate = new Activity(wrongDate, 5.0, user, task);
+			fail("OperationNotAllowedException should have been thrown"); 
+		} catch (OperationNotAllowedException e) {
+			assertEquals(e.getMessage(), "Date must have the format yyyy-MM-dd"); 
+			assertEquals(e.getOperation(),"Set date"); 
+		}
+		
+		assertEquals(task.getActivities().size(), 1);
+		
+	}	
+	
+	// TestEditRegisteredTime
+	
+	@Test 
+	public void testSelectActivity() throws Exception{
+		Project project = new Project("p1","2015-01", "2015-04");
+		Task task = new Task(project, "taskname", 5.5,"2015-02", "2015-03"); 
+		User user = new User("dev");
+		Activity activity = new Activity("2015-01-01", 5.0, user, task);
+		
+		//Test activity is in task
+		assertEquals(task.getActivityById(activity.getId()), activity); 
+		
+		//Test activity is not in another task
+		Task wrongTask = new Task(project, "taskname1", 5.5,"2015-02", "2015-03"); 
+		assertEquals(wrongTask.getActivityById(activity.getId()), null); 
+	}
+	
+	@Test
+	public void testEditActivity() throws Exception{
+		Project project = new Project("p1","2015-01", "2015-04");
+		Task task = new Task(project, "taskname", 5.5,"2015-02", "2015-03"); 
+		User user = new User("dev");
+		Activity activity = new Activity("2015-01-01", 5.0, user, task);
+		
+		Double dur = 1.5;
+		
+		// Test set a duration divisable by 0.5 AND not 0
+		activity.setDuration(dur);
+		assertEquals(activity.getDuration(), dur);
+		
+		// Test set a duration NOT divisable by 0.5
+		dur = 1.7;
+		try {
+			activity.setDuration(dur);
+		} catch(OperationNotAllowedException e){
+			assertEquals(e.getMessage(), "Activity duration must be divisable by 0.5 AND not 0");
+			assertEquals(e.getOperation(), "Set duration");
+		}
+		
+		// Test set a duration to 0
+		dur = 0.0;
+		try {
+			activity.setDuration(dur);
+		} catch(OperationNotAllowedException e){
+			assertEquals(e.getMessage(), "Activity duration must be divisable by 0.5 AND not 0");
+			assertEquals(e.getOperation(), "Set duration");
+		}
+	}
+
+	
+
 }
